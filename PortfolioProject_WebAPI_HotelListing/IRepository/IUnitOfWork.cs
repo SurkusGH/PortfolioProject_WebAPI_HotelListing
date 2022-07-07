c76c1df4f42ff09e732cd70ef93dec2213c0e742
SurkusGH <v.surkus@me.com>
2022-07-07 16:32:39 +0300

    +IGenericRepository +IUnitOfWork +DependencyInjection

---
+++ PortfolioProject_WebAPI_HotelListing/IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using PortfolioProject_WebAPI_HotelListing.DataModels;
+using System;
+using System.Threading.Tasks;
+
+namespace PortfolioProject_WebAPI_HotelListing.IRepository
+{
+    public interface IUnitOfWork : IDisposable // <- this interface allows for multiple actions to be done in one go
+    {                                          //    since repositories only modifie data in a program instance, dont save to db
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
---
+++ PortfolioProject_WebAPI_HotelListing/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using PortfolioProject_WebAPI_HotelListing.DataAccess;
+using PortfolioProject_WebAPI_HotelListing.DataModels;
+using PortfolioProject_WebAPI_HotelListing.IRepository;
+using System;
+using System.Threading.Tasks;
+
+namespace PortfolioProject_WebAPI_HotelListing.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Country> Countries => _countries ??= new GenericRepository<Country>(_context); // if its empty, return new
+
+        public IGenericRepository<Hotel> Hotels => _hotels ??= new GenericRepository<Hotel>(_context); // if its empty, return new
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this); // GarbageColector
+        }
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
